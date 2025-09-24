d651f9cf17e16a235e606cf5848616199aecf0de
Aryan Verma <av868400@gmail.com>
2025-09-24 12:47:14 +0530

    New Exercise Added

--- Extraction_of_Digit/Exercise5.py
+++ Extraction_of_Digit/Exercise5.py
@@ -1,5 +1,15 @@
-n=566
+#armstrong no. lets suppose a numnber 153 so if we take cube of 1 + cube of 5 + cube of 3 so, 1+125+27 =153 so its a armostring number.
+# yaha cube isliye kiya kiuki 3 number thy agr 2 number hoty to square krty aur 4 hoty to power 4 hoti
+n=153
 num=n
+nod=len(str(num))
+total=0
 while num>0:
     ld=num%10
-     
+    total=total+(ld**nod)
+    num=num//10
+if n==total:
+    print("an armstrong number")
+else:
+    print("not armstrong")
+    
