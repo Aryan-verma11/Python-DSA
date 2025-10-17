ef25e1de5256009aa61cb76fea78f76d7e27e7c1
Aryan Verma <av868400@gmail.com>
2025-10-17 22:49:45 +0530

    fun added

---
+++ fun/ludo.py
@@ -0,0 +1,7 @@
+import random
+import time
+
+dice=random.randint(1,6)
+                                                                                                    
+time.sleep(2)
+print(dice)
